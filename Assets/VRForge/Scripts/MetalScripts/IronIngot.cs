9ef48d26ca8e0937739c21a75537d25b9535c49a
CoyoteStarch <jaydenwang@nevada.unr.edu>
2017-04-05 20:56:07 -0700

    Commit without VRTK Examples
    A commit to push HotF without VRTK Examples part of the project

---
+++ Assets/VRForge/Scripts/MetalScripts/IronIngot.cs
@@ -0,0 +1,53 @@
+﻿namespace VRTK
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class IronIngot : VRTK_InteractableObject
+    {
+
+        private GameObject ingot;
+        private Color targetColor = Color.yellow;
+        private Color initialColor;
+
+        // Use this for initialization
+        void Start()
+        {
+            ingot = GameObject.Find("ingot").gameObject;
+            initialColor = ingot.GetComponent<MeshRenderer>().material.color;
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+            changeColor();
+        }
+
+        private void changeColor()
+        {
+            if(inForge())
+            {
+
+                Color currentColor = Color.Lerp(initialColor, targetColor, Mathf.PingPong(Time.time, 1));
+                ingot.GetComponent<MeshRenderer>().material.color = currentColor;
+
+                if(currentColor == targetColor)
+                {
+                    var lastColor = initialColor;
+                    initialColor = targetColor;
+                    targetColor = lastColor;
+                }
+            }
+
+        }
+
+        private bool inForge()
+        {
+
+            return true;
+        }
+
+    }
+}
